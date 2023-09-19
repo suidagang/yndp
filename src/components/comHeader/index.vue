522c4bc4e2c60491186ad145037241110cd8c2ff
suigang <244153653@qq.com>
2023-09-19 20:32:13 +0800

    feat:家里面提交代码测试

--- src/components/comHeader/index.vue
+++ src/components/comHeader/index.vue
@@ -2,9 +2,12 @@
   <div class="comheader">
     <div class="comheader-title">智慧养牛数字供应链平台</div>
     <div class="comheader-tab">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <div class="text">养牛场基础信息简报</div>
+        <div class="active"></div>
+>      </div>
+      <div>自营牛场管理</div>
+      <div>合作农户管理</div>
     </div>
     <div class="bottom-line"></div>
   </div>
@@ -14,7 +17,7 @@
 export default {
   data() {
     return {
-
+     
     }
   },
   components: {
@@ -39,6 +42,9 @@ export default {
       top: 60px;
     }
     &-title{
+      position: absolute;
+      left: 0;
+      top: 14px;
       width: 507px;
       height: 40px;
       line-height: 40px;
@@ -50,9 +56,36 @@ export default {
       font-family: YouSheBiaoTiHei;
       font-weight: 400;
       color: #C3E3FE;
-      margin-top: 14px;
       padding-left: 35px;
       box-sizing: border-box;
+      cursor: pointer;
     }
+    &-tab{
+      display: flex;
+      justify-content: center;
+      width: 483px;
+      position: absolute;
+      left: 635px;
+      top: 30px;
+      color: #ffffff;
+      cursor: pointer;
+      >div{
+        flex: 1;
+        text-align: center;
+        .text{
+          font-size: 16px;
+          color: #FFFFFF;
+          text-shadow: 0px 7px 3px #02274C;
+          background: linear-gradient(0deg, rgba(0,169,255,0.83) 0%, rgba(232,254,255,0.83) 100%);
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+        }
+      }
+      .active{
+        width: 161px;
+        height: 29px;
+        background: url("../../assets/img/tab_active.png") center no-repeat;
+      }
+    } 
   }
 </style>
