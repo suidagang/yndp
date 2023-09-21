bd6070e64a6eaedb4994b1ed2b8da4d500a2552e
suigang <244153653@qq.com>
2023-09-21 20:57:12 +0800

    feat: 21家里最后提交

--- src/views/ncgl/ncgl.vue
+++ src/views/ncgl/ncgl.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="container">
-    牛场管理
+  <div class="container-box">
+    <topOne class="top-one" />
+    <topTwo class="top-two" />
+    <bottomOne class="bottom-one" />
   </div>
 </template>
 
 <script>
+import topOne from './components/topOne.vue'
+import topTwo from './components/topTwo.vue'
+import bottomOne from './components/bottomOne.vue'
 export default {
   data() {
     return {
@@ -12,13 +17,33 @@ export default {
     }
   },
   components: {
-
+    topOne,
+    topTwo,
+    bottomOne
   }
 }
 </script>
 
 <style scoped lang="less">
-  .container{
-    color: #fff;
+  .container-box{
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    .top-one{
+      position: absolute;
+      left: 70px;
+      top: 58px;
+    }
+    .top-two{
+      position: absolute;
+      left: 987px;
+      top: 58px;
+    }
+    .bottom-one{
+      position: absolute;
+      left: 70px;
+      top: 551px;
+    }
   }
 </style>
