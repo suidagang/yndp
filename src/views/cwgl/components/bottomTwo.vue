9afd0513acbd5a9e0f7fa04570404182fe5e7a40
suigang <244153653@qq.com>
2023-09-23 19:12:14 +0800

    feat: 测试

--- src/views/cwgl/components/bottomTwo.vue
+++ src/views/cwgl/components/bottomTwo.vue
@@ -1,80 +1,111 @@
 <template>
-  <div class="container-box">
-    <comTitle  tabTitle="毛利统计" />
+  <div class="container">
+    <comTitle tabTitle="毛利统计" />
     <div class="content">
-      
+      <comEcharts class="echarts" ref="chart" :options="options" />
     </div>
   </div>
 </template>
 
 <script>
-import comTitle from '@/components/comBoxHead/index.vue'
+import comTitle from "@/components/comBoxHead/index.vue";
+import comEcharts from "@/components/ComEcharts/index.vue";
 export default {
   data() {
     return {
-    
-    }
+      options: {
+        title: {
+          text: "收入/万元",
+          top: "30%",
+          textAlign: "center",
+          left: "18%",
+          textStyle: {
+            color: " #86B2D7",
+            fontSize: 14,
+            fontWeight: "400",
+            lineHeight: 26,
+          },
+          subtext: "455",
+          subtextStyle: {
+            color: "#2ACFF1",
+            fontSize: 22,
+            fontWeight: "400",
+            fontFamily: "PangMenZhengDao",
+          },
+        },
+        legend: [
+          {
+            orient: "vertical",
+            left: "40%",
+            top: "17%",
+            icon: 'circle', // 图例样式为圆形
+            itemGap: 30,
+            textStyle: {
+              color: "#B7C9E5",
+              fontSize: 14,
+            },
+            data: ["XXXX收入1", "XXXX收入2",'XXXX收入3'],
+          },
+          {
+            orient: "vertical",
+            left: "60%",
+            icon: 'circle', // 图例样式为圆形
+            top: "17%",
+            itemGap: 30,
+            textStyle: {
+              color: "#B7C9E5",
+              fontSize: 14,
+            },
+            data: ["XXXX收入4", "XXXX收入5"],
+          },
+        ],
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            center: ['18%', '50%'],
+            radius: ["60%", "90%"],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: "center",
+            },
+            emphasis: {
+              label: {
+                show: false,
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              { value: 1048, name: "XXXX收入1" },
+              { value: 735, name: "XXXX收入2" },
+              { value: 580, name: "XXXX收入3" },
+              { value: 484, name: "XXXX收入4" },
+              { value: 300, name: "XXXX收入5" },
+            ],
+          },
+        ],
+      },
+    };
   },
   components: {
-    comTitle
+    comTitle,
+    comEcharts,
   },
-  methods:{
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less">
-  .container-box{
-    width: 500px;
-    overflow: hidden;
+.content {
+  width: 680px;
+  overflow: hidden;
+  padding-top: 10px;
+  .echarts {
+    width: 680px;
+    height: 170px;
   }
-  .content{
-    width: 100%;
-    padding-left: 39px;
-    padding-top: 43px;
-    box-sizing: border-box;
-    overflow: hidden;
-    &-list{
-      float: left;
-      display: flex;
-      align-items: center;
-      margin-bottom: 40px;
-      width: 37%;
-      >.img{
-        width: 58px;
-        height: 60px;
-        background: url("../../../assets/img/srtj.png") center no-repeat;
-        margin-right: 19px;
-      }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.sl-img{
-        background: url("../../../assets/img/slcl.png") center no-repeat;
-      }
-      >.right-text{
-        font-size: 14px;
-        font-family: Source Han Sans SC;
-        font-weight: 400;
-        color: #A2D1FF;
-      }
-    }
-  }
-  .number-text{
-    margin-top: 14px;
-    font-size: 26px;
-    font-family: PangMenZhengDao;
-    font-weight: 400;
-    color: #00FFE4;
-    background: linear-gradient(0deg, #45CDFF 0%, #E5F9FF 100%);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  .unit-text{
-    font-size: 14px;
-  }
+}
 </style>
