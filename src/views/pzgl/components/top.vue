aa0b324f0aa0aacd8f1d932219ce5216cdfa42c8
suigang <244153653@qq.com>
2023-09-23 20:40:39 +0800

    feat: 完善

--- src/views/pzgl/components/top.vue
+++ src/views/pzgl/components/top.vue
@@ -1,107 +1,301 @@
 <template>
-  <div class="container-box">
-    <comTitle  tabTitle="牛种信息" />
+  <div class="nzxx-box">
+    <comTitle tabTitle="牛种信息" />
     <div class="content">
-      <div class="content-list">
-        <div class="img"></div>
-        <div class="right-text">
-          <div>肉牛统计</div>
-          <div class="number-text">9600<span class="unit-text">元</span></div>
-        </div>
-      </div>
-      <div class="content-list">
-        <div class="img zc-img"></div>
-        <div class="right-text">
-          <div>支出统计</div>
-          <div  class="number-text">9600<span class="unit-text">元</span></div>
-        </div>
-      </div>
-      <div class="content-list">
-        <div class="img cl-img"></div>
-        <div class="right-text">
-          <div>存栏统计</div>
-          <div class="number-text">9600<span class="unit-text">个</span></div>
-        </div>
-      </div>
-      <div class="content-list">
-        <div class="img sl-img"></div>
-        <div class="right-text">
-          <div>饲料库存统计</div>
-          <div class="number-text">9600<span class="unit-text">吨</span></div>
-        </div>
-      </div>
+      <comEcharts class="echarts" ref="chart" :options="options" />
     </div>
   </div>
 </template>
 
 <script>
-import comTitle from '@/components/comBoxHead/index.vue'
+import comTitle from "@/components/comBoxHead/index.vue";
+import comEcharts from "@/components/ComEcharts/index.vue";
+let xData = [
+  "xx街道党支部",
+  "xx街道党支部",
+  "xx街道党支部",
+  "xx街道党支部",
+  "xx街道党支部",
+];
+let data = [146, 274, 308, 418, 319];
+let data2 = [234, 135, 237, 135, 237];
+const colorArr = [
+  "rgba(15, 201, 248)",
+  "rgba(30, 50, 221)",
+  "rgba(61, 101, 249)",
+  "rgba(95, 236, 251)",
+];
+const colorArr2 = [
+  "rgba(19, 219, 182)",
+  "rgb(17, 197, 210)",
+  "rgb(17, 197, 210)",
+  "rgb(88, 235, 131)",
+];
+let color = {
+   type: "linear",
+   x: 0,
+   x2: 0,
+   y: 0,
+   y2: 1,
+   colorStops: [
+      {
+         offset: 0,
+         color: colorArr[0]
+      },
+      {
+         offset: 1,
+         color: colorArr[1]
+      }
+   ]
+};
+let color2 = {
+   type: "linear",
+   x: 0,
+   x2: 0,
+   y: 0,
+   y2: 1,
+   colorStops: [
+      {
+         offset: 0,
+         color: colorArr2[0]
+      },
+      {
+         offset: 1,
+         color: colorArr2[1]
+      }
+   ]
+};
+let barWidth = 16;
 export default {
   data() {
     return {
-    
-    }
+      options: {
+        grid: {
+          left: "5%",
+          top: "10%",
+          right: "0%",
+          bottom: "10%",
+        },
+        xAxis: {
+          data: ["祝贺", "中国航天", "中国", "太空", "祝福"],
+          axisLine: {
+            show: true, //隐藏X轴轴线
+            lineStyle: {
+              color: "#2D3C5C",
+              width: 1,
+            },
+          },
+          axisTick: {
+            show: false, //隐藏X轴刻度
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: "#86B2D7", //X轴文字颜色
+              fontSize: 14,
+            },
+            interval: 0,
+          },
+        },
+        yAxis: [
+          {
+            type: "value",
+            nameGap: 30, // 将标题和轴线之间的距离设置为 30 像素
+            // nameLocation: 'middle', // 将标题位置设置为中间
+            // 其他配置选项...
+
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: "#2D3C5C",
+                width: 1,
+              },
+            },
+            axisTick: {
+              show: false,
+            },
+            axisLine: {
+              show: true, //隐藏X轴轴线
+              lineStyle: {
+                color: "#163a5f",
+                width: 1,
+              },
+            },
+            axisLabel: {
+              show: true,
+              textStyle: {
+                color: "#86B2D7",
+                fontSize: 12,
+              },
+            },
+          },
+          {
+            type: "value",
+            name: "",
+            nameTextStyle: {
+              color: "#BDD8FB",
+              fontSize: 12,
+            },
+            splitLine: {
+              show: false,
+              lineStyle: {
+                width: 1,
+                color: "#CED2DB",
+              },
+            },
+            axisTick: {
+              show: false,
+            },
+            axisLine: {
+              show: false, //隐藏X轴轴线
+              lineStyle: {
+                color: "#163a5f",
+                width: 2,
+              },
+            },
+            axisLabel: {
+              show: false,
+              textStyle: {
+                color: "#797A7F",
+                fontSize: 12,
+              },
+            },
+          },
+        ],
+        series: [
+          {
+            z: 1,
+            name: "党员数",
+            type: "bar",
+            barWidth: barWidth,
+            barGap: "0%",
+            data: data,
+            itemStyle: {
+              normal: {
+                color: color,
+                label: {
+                  show: false,
+                },
+              },
+            },
+          },
+          {
+            z: 2,
+            name: "党员数",
+            type: "pictorialBar",
+            data: data.map((item) => item + 10),
+            symbol: "diamond",
+            symbolOffset: ["-70%", "50%"],
+            symbolSize: [barWidth, 10],
+            itemStyle: {
+              normal: {
+                color: colorArr[2],
+              },
+            },
+            tooltip: {
+              show: false,
+            },
+          },
+          {
+            z: 3,
+            name: "党员数",
+            type: "pictorialBar",
+            symbolPosition: "end",
+            data: data,
+            symbol: "diamond",
+            symbolOffset: ["-70%", "-60%"],
+            symbolSize: [barWidth - 1, (10 * (barWidth - 1)) / barWidth],
+            itemStyle: {
+              normal: {
+                borderWidth: 2,
+                color: colorArr[3],
+              },
+            },
+            tooltip: {
+              show: false,
+            },
+          },
+          {
+            z: 1,
+            name: "网格员数",
+            type: "bar",
+            barWidth: barWidth,
+            barGap: "30%",
+            data: data2,
+            itemStyle: {
+              normal: {
+                color: color2,
+                label: {
+                  show: false,
+                },
+              },
+            },
+          },
+          {
+            z: 2,
+            name: "网格员数",
+            type: "pictorialBar",
+            data: data2.map((item) => item + 10),
+            symbol: "diamond",
+            symbolOffset: ["70%", "50%"],
+            symbolSize: [barWidth, 10],
+            itemStyle: {
+              normal: {
+                color: colorArr2[2],
+              },
+            },
+            tooltip: {
+              show: false,
+            },
+          },
+          {
+            z: 3,
+            name: "网格员数",
+            type: "pictorialBar",
+            symbolPosition: "end",
+            // barCategoryGap : "90%",
+            data: data2,
+            symbol: "diamond",
+            symbolOffset: ["70%", "-50%"],
+            symbolSize: [barWidth - 1, (10 * (barWidth - 1)) / barWidth],
+            itemStyle: {
+              normal: {
+                borderWidth: 2,
+                color: colorArr2[3],
+              },
+            },
+            tooltip: {
+              show: false,
+            },
+          },
+        ],
+      },
+    };
   },
   components: {
-    comTitle
+    comTitle,
+    comEcharts,
   },
-  methods:{
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less">
-  .container-box{
-    width: 500px;
-    overflow: hidden;
-  }
-  .content{
+.nzxx-box {
+  width: 1350px;
+  height: 100%;
+  overflow: hidden;
+}
+.content {
+  width: 100%;
+  height: 380px;
+  overflow: hidden;
+  padding-left: 10px;
+  box-sizing: border-box;
+  .echarts {
     width: 100%;
-    padding-left: 39px;
-    padding-top: 43px;
-    box-sizing: border-box;
-    overflow: hidden;
-    &-list{
-      float: left;
-      display: flex;
-      align-items: center;
-      margin-bottom: 40px;
-      width: 37%;
-      >.img{
-        width: 58px;
-        height: 60px;
-        background: url("../../../assets/img/srtj.png") center no-repeat;
-        margin-right: 19px;
-      }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.sl-img{
-        background: url("../../../assets/img/slcl.png") center no-repeat;
-      }
-      >.right-text{
-        font-size: 14px;
-        font-family: Source Han Sans SC;
-        font-weight: 400;
-        color: #A2D1FF;
-      }
-    }
+    height: 330px;
   }
-  .number-text{
-    margin-top: 14px;
-    font-size: 26px;
-    font-family: PangMenZhengDao;
-    font-weight: 400;
-    color: #00FFE4;
-    background: linear-gradient(0deg, #45CDFF 0%, #E5F9FF 100%);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  .unit-text{
-    font-size: 14px;
-  }
+}
 </style>
