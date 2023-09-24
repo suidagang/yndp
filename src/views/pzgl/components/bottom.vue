b830121dd7c81245099722c6f63a40d9ff31f3ef
suigang <244153653@qq.com>
2023-09-24 09:07:27 +0800

    feat:  完善

--- src/views/pzgl/components/bottom.vue
+++ src/views/pzgl/components/bottom.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="container-box">
-    <comTitle  tabTitle="繁育管理" />
+    <comTitle tabTitle="繁育管理" />
     <div class="content">
-      <div class="content-list">
-        <div class="img"></div>
-        <div class="right-text">
-          <div>肉牛统计</div>
-          <div class="number-text">9600<span class="unit-text">元</span></div>
+      <div class="content-top">
+        <div class="content-top-list">
+          <div class="left">
+            <div class="green">6</div>
+            <div>冻精种类/种</div>
+          </div>
+          <div class="line"></div>
         </div>
-      </div>
-      <div class="content-list">
-        <div class="img zc-img"></div>
-        <div class="right-text">
-          <div>支出统计</div>
-          <div  class="number-text">9600<span class="unit-text">元</span></div>
+        <div class="content-top-list">
+          <div class="left">
+            <div class="blue">99</div>
+            <div>冻精总数/细管-支</div>
+          </div>
+          <div class="line"></div>
         </div>
-      </div>
-      <div class="content-list">
-        <div class="img cl-img"></div>
-        <div class="right-text">
-          <div>存栏统计</div>
-          <div class="number-text">9600<span class="unit-text">个</span></div>
+        <div class="content-top-list">
+          <div class="left">
+            <div class="blue1">47</div>
+            <div>云岭肉牛/细管-支</div>
+          </div>
+          <div class="line"></div>
         </div>
-      </div>
-      <div class="content-list">
-        <div class="img sl-img"></div>
-        <div class="right-text">
-          <div>饲料库存统计</div>
-          <div class="number-text">9600<span class="unit-text">吨</span></div>
+        <div class="content-top-list">
+          <div class="left">
+            <div class="blue">16</div>
+            <div>西门塔尔/细管-支</div>
+          </div>
+          <div class="line"></div>
+        </div>
+        <div class="content-top-list">
+          <div class="left">
+            <div class="green">27</div>
+            <div>鲁西黄牛/细管-支</div>
+          </div>
         </div>
       </div>
     </div>
@@ -35,73 +43,72 @@
 </template>
 
 <script>
-import comTitle from '@/components/comBoxHead/index.vue'
+import comTitle from "@/components/comBoxHead/index.vue";
 export default {
   data() {
-    return {
-    
-    }
+    return {};
   },
   components: {
-    comTitle
+    comTitle,
   },
-  methods:{
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less">
-  .container-box{
-    width: 100%;
+.container-box {
+  width: 100%;
+  overflow: hidden;
+}
+.content {
+  width: 100%;
+  padding-left: 39px;
+  padding-top: 43px;
+  box-sizing: border-box;
+  overflow: hidden;
+  &-top {
+    height: 55px;
+    display: flex;
+    align-items: center;
     overflow: hidden;
-  }
-  .content{
-    width: 100%;
-    padding-left: 39px;
-    padding-top: 43px;
-    box-sizing: border-box;
-    overflow: hidden;
-    &-list{
-      float: left;
+    &-list {
       display: flex;
       align-items: center;
-      margin-bottom: 40px;
-      width: 37%;
-      >.img{
-        width: 58px;
-        height: 60px;
-        background: url("../../../assets/img/srtj.png") center no-repeat;
-        margin-right: 19px;
+      overflow: hidden;
+      margin-right: 17px;
+      > .left {
+        margin-bottom: 15px;
+        overflow: hidden;
+        > div:nth-of-type(1) {
+          font-size: 30px;
+          font-family: PangMenZhengDao;
+          font-weight: 400;
+
+          text-align: center;
+        }
+        > .green {
+          color: #21db93;
+        }
+        > .blue {
+          color: #00bdff;
+        }
+        .blue1 {
+          color: #00f6ff;
+        }
+        > div:nth-of-type(2) {
+          font-size: 14px;
+          font-family: Source Han Sans SC;
+          font-weight: 400;
+          color: #86b2d7;
+        }
       }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.zc-img{
-        background: url("../../../assets/img/zclx.png") center no-repeat;
-      }
-      >.sl-img{
-        background: url("../../../assets/img/slcl.png") center no-repeat;
-      }
-      >.right-text{
-        font-size: 14px;
-        font-family: Source Han Sans SC;
-        font-weight: 400;
-        color: #A2D1FF;
+      > .line {
+        width: 2px;
+        height: 52px;
+        background: url("../../../assets/img/pzgl/line.png") center no-repeat;
+        margin-left: 24px;
       }
     }
   }
-  .number-text{
-    margin-top: 14px;
-    font-size: 26px;
-    font-family: PangMenZhengDao;
-    font-weight: 400;
-    color: #00FFE4;
-    background: linear-gradient(0deg, #45CDFF 0%, #E5F9FF 100%);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  .unit-text{
-    font-size: 14px;
-  }
+}
 </style>
