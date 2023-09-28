be16340c25abe9c4ae21863a6e8371af3ce7718e
suigang <suigang@css.com.cn>
2023-09-28 11:02:15 +0800

    完善接口

--- src/http/config.js
+++ src/http/config.js
@@ -4,5 +4,7 @@ const config = {
   yncjcxxjbCwhztj: "/cattle/basicInfoReport/costCount", //【养牛场基础信息简报】财务汇总统计
   farmList: "/cattle/farm/farmList",//【养牛场基础信息简报】查询自营牛场有哪些牛场
   farmSelfBriefing:"/cattle/farm/selfBriefing",//【养牛场基础信息简报】自营牛场简报 传对应的 （farmList）牛场id 参数:farmId
+  cattleMillList:"/cattle/farm/cattleMillList",//【养牛场基础信息简报】查询合作农户列表
+  cooperateBriefing:"/cattle/farm/cooperateBriefing",//【养牛场基础信息简报】合作农户简报 请求传cattleMillList接口的id参数名： cooperateId
 };
 export default config;
--- src/views/yncjcxxjb/components/leftThree.vue
+++ src/views/yncjcxxjb/components/leftThree.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="container">
-    <comTitle  :tabList='tabList' tabTitle="合作养殖汇总简报" :tabspacing="26" @changeTab = "changeTab" />
+    <comTitle
+      :tabList="tabList"
+      tabTitle="合作养殖汇总简报"
+      :tabspacing="26"
+      @changeTab="changeTab"
+    />
     <div class="content">
       <div class="content-list">
         <div class="img img-cl"></div>
         <div class="content-list-right">
           <div class="name">存栏总数/个</div>
-          <div class="number">1000</div>
+          <div class="number">{{ clnum }}</div>
         </div>
       </div>
       <div class="content-list">
         <div class="img img-cmn"></div>
         <div class="content-list-right">
           <div class="name">能繁母牛数/头</div>
-          <div class="number">1000</div>
+          <div class="number">{{ nfmnnum }}</div>
         </div>
       </div>
       <div class="content-list">
         <div class="img img-llzl"></div>
         <div class="content-list-right">
           <div class="name">存栏总重量/吨</div>
-          <div class="number">1000</div>
+          <div class="number">{{ clzzl }}</div>
         </div>
       </div>
       <div class="content-list">
         <div class="img img-slsc"></div>
         <div class="content-list-right">
           <div class="name">饲料剩余重量/吨</div>
-          <div class="number">1000</div>
+          <div class="number">{{ slsyzl }}</div>
         </div>
       </div>
       <div class="content-list">
         <div class="img img-nd"></div>
         <div class="content-list-right">
           <div class="name">牛犊数/头</div>
-          <div class="number">1000</div>
+          <div class="number">{{ ndnum }}</div>
         </div>
       </div>
       <div class="content-list">
         <div class="img img-ycn"></div>
         <div class="content-list-right">
           <div class="name">青牛数/头</div>
-          <div class="number">1000</div>
+          <div class="number">{{ qnnum }}</div>
         </div>
       </div>
     </div>
@@ -49,87 +54,123 @@
 </template>
 
 <script>
-import comTitle from '@/components/comBoxHead/index.vue'
+import comTitle from "@/components/comBoxHead/index.vue";
+import config from "@/http/config";
 export default {
   data() {
     return {
-      tabList: [
-        { name: "A合作社", active: true },
-        { name: "B合作社", active: false }
-      ],
-    }
+      tabList: [],
+      clnum: 0, //存栏
+      clzzl: 0, //存栏总重量
+      ndnum: 0, // 牛犊数
+      nfmnnum: 0, //能繁母牛
+      qnnum: 0, //青牛数
+      slsyzl: 0, // 饲料剩余重量
+    };
   },
   components: {
-    comTitle
+    comTitle,
   },
-  methods:{
-    changeTab(num){
-      console.log(num,'---num')
-    }
-  }
-}
+  created() {
+    this.getCattleMillList();
+  },
+  methods: {
+    getCattleMillList() {
+      this.$get(config.cattleMillList).then((res) => {
+        let dataList = res.data;
+        dataList.map((item, index) => {
+          if (index === 0) {
+            item.active = true;
+          } else {
+            item.active = false;
+          }
+          item.name = item.millName;
+        });
+        this.getCooperateBriefing(dataList[0].id);
+        this.tabList = dataList;
+        this.tabList.length = 2;
+      });
+    },
+    getCooperateBriefing(id) {
+      const params = {
+        cooperateId: id,
+      };
+      this.$get(config.cooperateBriefing, params).then((res) => {
+        this.clnum = res.data.clnum || 0;
+        this.clzzl = res.data.clzzl || 0;
+        this.ndnum = res.data.ndnum || 0;
+        this.nfmnnum = res.data.nfmnnum || 0;
+        this.qnnum = res.data.qnnum || 0;
+        this.slsyzl = res.data.slsyzl || 0;
+      });
+    },
+    changeTab(num, item) {
+      this.getCooperateBriefing(item.id);
+    },
+  },
+};
 </script>
 
 <style scoped lang="less">
-  .container{
-    width: 465px;
+.container {
+  width: 465px;
+  overflow: hidden;
+}
+.content {
+  width: 100%;
+  overflow: hidden;
+  overflow: hidden;
+  padding-top: 25px;
+  padding-left: 8px;
+  &-list {
+    width: 50%;
+    float: left;
     overflow: hidden;
-  }
-  .content{
-    width: 100%;
-    overflow: hidden;
-    overflow: hidden;
-    padding-top: 25px;
-    padding-left: 8px;
-    &-list{
-      width: 50%;
-      float: left;
+    display: flex;
+    align-items: center;
+    > .img {
+      width: 98px;
+      height: 98px;
+    }
+    > .img-cl {
+      background: url("../../../assets/img/yncjcxxjb/cl.png") center no-repeat;
+    }
+    > .img-cmn {
+      background: url("../../../assets/img/yncjcxxjb/cmn.png") center no-repeat;
+    }
+    > .img-llzl {
+      background: url("../../../assets/img/yncjcxxjb/llzl.png") center no-repeat;
+    }
+    > .img-slsc {
+      background: url("../../../assets/img/yncjcxxjb/slsc.png") center no-repeat;
+    }
+    > .img-nd {
+      background: url("../../../assets/img/yncjcxxjb/nd.png") center no-repeat;
+    }
+    > .img-ycn {
+      background: url("../../../assets/img/yncjcxxjb/ycn.png") center no-repeat;
+    }
+    &-right {
+      margin-top: -7px;
       overflow: hidden;
-      display: flex;
-      align-items: center;
-      >.img{
-        width: 98px;
-        height: 98px;
+      > .name {
+        font-size: 14px;
+        font-family: Source Han Sans SC;
+        font-weight: 400;
+        color: #a2d1ff;
+        margin-bottom: 7px;
       }
-      >.img-cl{
-        background: url("../../../assets/img/yncjcxxjb/cl.png") center no-repeat;
-      }
-      >.img-cmn{
-        background: url("../../../assets/img/yncjcxxjb/cmn.png") center no-repeat;
-      }
-      >.img-llzl{
-        background: url("../../../assets/img/yncjcxxjb/llzl.png") center no-repeat;
-      }
-      >.img-slsc{
-        background: url("../../../assets/img/yncjcxxjb/slsc.png") center no-repeat;
-      }
-      >.img-nd{
-        background: url("../../../assets/img/yncjcxxjb/nd.png") center no-repeat;
-      }
-      >.img-ycn{
-        background: url("../../../assets/img/yncjcxxjb/ycn.png") center no-repeat;
-      }
-      &-right{
-        margin-top: -7px;
-        overflow: hidden;
-        >.name{
-          font-size: 14px;
-          font-family: Source Han Sans SC;
-          font-weight: 400;
-          color: #A2D1FF;
-          margin-bottom: 7px;
-        }
-        >.number{
-          font-size: 26px;
-          font-family: PangMenZhengDao;
-          font-weight: 400;
-          color: #00FFE4;
+      > .number {
+        font-size: 26px;
+        font-family: PangMenZhengDao;
+        font-weight: 400;
+        color: #00ffe4;
 
-          background: linear-gradient(0deg, #45CDFF 0%, #E5F9FF 100%);
-          background-clip: text;
-          -webkit-text-fill-color: transparent;
-        }
+        background: linear-gradient(0deg, #45cdff 0%, #e5f9ff 100%);
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
       }
     }
   }
+}
 </style>
